d3275eed787140c1ba4ecb0eab905abec49ebfa9
mareksadek <mareksadek45@gmail.com>
2025-04-02 13:16:57 +0200

    update digital_clock.vhd

--- digital_clock/digital_clock.srcs/sources_1/new/digital_clock.vhd
+++ digital_clock/digital_clock.srcs/sources_1/new/digital_clock.vhd
@@ -5,8 +5,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity digital_clock is
---  Port ( );
+    Port ( 
+        clk : in std_logic;
+        rst : in std_logic;
+        set_hours : in std_logic_vector(1 downto 0);
+        set_minutes : in std_logic_vector(1 downto 0);
+        hours : out std_logic;
+        minutes : out std_logic;
+        seconds : out std_logic;
+        display: out std_logic_vector(6 downto 0)
+    );
+    
 end digital_clock;
 
 architecture Behavioral of digital_clock is
