5873068873d17e1a7d31535425db70e275555707
Matěj Ševčík <257261@vutbr.cz>
2025-04-09 12:15:15 +0200

    add proces

--- digital_clock/digital_clock.srcs/sources_1/new/digital_clock.vhd
+++ digital_clock/digital_clock.srcs/sources_1/new/digital_clock.vhd
@@ -1,9 +1,11 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -16,17 +18,76 @@ entity digital_clock is
         rst : in std_logic;
         set_hours : in std_logic_vector(1 downto 0);
         set_minutes : in std_logic_vector(1 downto 0);
-        hours : out std_logic;
-        minutes : out std_logic;
-        seconds : out std_logic;
+        hours : out std_logic_vector(5 downto 0);
+        minutes : out std_logic_vector(5 downto 0);
+        seconds : out std_logic_vector(5 downto 0);
         display: out std_logic_vector(6 downto 0)
     );
     
 end digital_clock;
 
 architecture Behavioral of digital_clock is
+    signal sec_count   : std_logic_vector(5 downto 0) := "000000"; -- sekundy
+    signal min_count   : std_logic_vector(5 downto 0) := "000000"; -- minuty
+    signal hr_count    : std_logic_vector(5 downto 0) := "000000"; -- hodiny
 
 begin
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            sec_count <= "000110";
+            min_count <= "000000";
+            hr_count  <= "000000";
+        elsif clk = '1' and clk'event then  -- Změna na hodinovém signálu
+            -- Inkrementace sekund
+            if sec_count = "111011" then  -- 59 sekund
+                sec_count <= "000000";
+                -- Inkrementace minut
+                if min_count = "111011" then  -- 59 minut
+                    min_count <= "000000";
+                    -- Inkrementace hodin
+                    if (hr_count = "10111") then -- 24h režim (23)
+                        hr_count <= "000000"; -- Reset na 0
+                    else
+                        hr_count <= hr_count + 1;
+                    end if;
+                else
+                    min_count <= min_count + 1;
+                end if;
+            else
+                sec_count <= sec_count + 1;
+            end if;
+        end if;
+    end process;
 
+    -- Nastavení hodin tlačítkem
+    process(set_hours)
+    begin
+        if set_hours(0) = '1' and set_hours'event then
+            if (hr_count = "1011") then  -- 12h režim
+                hr_count <= "000001";  -- Reset na 1
+            elsif (hr_count = "10111") then  -- 24h režim
+                hr_count <= "000000";  -- Reset na 0
+            else
+                hr_count <= hr_count + 1;
+            end if;
+        end if;
+    end process;
 
+    -- Nastavení minut tlačítkem
+    process(set_minutes)
+    begin
+        if set_minutes(0) = '1' and set_minutes'event then
+            if min_count = "111011" then  -- 59 minut
+                min_count <= "000000";
+            else
+                min_count <= min_count + 1;
+            end if;
+        end if;
+    end process;
+
+    -- Výstupy
+    hours <= hr_count;
+    minutes <= min_count;
+    seconds <= sec_count;
 end Behavioral;
