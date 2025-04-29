3e10e1e677e74979b3e7c9cb24d23adb2c460bd7
Matěj Ševčík <257261@vutbr.cz>
2025-04-29 23:34:07 +0200

    update nexys and tb

--- digital_clock/digital_clock.srcs/sim_1/new/tb_digital_clock.vhd
+++ digital_clock/digital_clock.srcs/sim_1/new/tb_digital_clock.vhd
@@ -1,27 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.04.2025 12:24:49
--- Design Name: 
--- Module Name: tb_digital_clock - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
--- Testbench automatically generated online
--- at https://vhdl.lapinoo.net
--- Generation date : Wed, 16 Apr 2025 10:18:44 GMT
--- Request id : cfwk-fed377c2-67ff84045c40b
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -31,102 +7,54 @@ end tb_digital_clock;
 architecture tb of tb_digital_clock is
 
     component digital_clock
-        port (clk               : in std_logic;
-              rst               : in std_logic;
-              set_hours_plus    : in std_logic_vector (1 downto 0);
-              set_hours_minus   : in std_logic_vector (1 downto 0);
-              set_minutes_plus  : in std_logic_vector (1 downto 0);
-              set_minutes_minus : in std_logic_vector (1 downto 0);
-              hours             : out std_logic_vector (5 downto 0);
-              minutes           : out std_logic_vector (5 downto 0);
-              seconds           : out std_logic_vector (5 downto 0));
+        port (clk     : in std_logic;
+              rst     : in std_logic;
+              hours   : out std_logic_vector (5 downto 0);
+              minutes : out std_logic_vector (5 downto 0);
+              seconds : out std_logic_vector (5 downto 0));
     end component;
 
-    signal clk               : std_logic;
-    signal rst               : std_logic;
-    signal set_hours_plus    : std_logic_vector (1 downto 0);
-    signal set_hours_minus   : std_logic_vector (1 downto 0);
-    signal set_minutes_plus  : std_logic_vector (1 downto 0);
-    signal set_minutes_minus : std_logic_vector (1 downto 0);
-    signal hours             : std_logic_vector (5 downto 0);
-    signal minutes           : std_logic_vector (5 downto 0);
-    signal seconds           : std_logic_vector (5 downto 0);
+    signal clk     : std_logic;
+    signal rst     : std_logic;
+    signal hours   : std_logic_vector (5 downto 0);
+    signal minutes : std_logic_vector (5 downto 0);
+    signal seconds : std_logic_vector (5 downto 0);
 
-    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    constant TbPeriod : time := 1 ns; 
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
 begin
 
     dut : digital_clock
-    port map (clk               => clk,
-              rst               => rst,
-              set_hours_plus    => set_hours_plus,
-              set_hours_minus   => set_hours_minus,
-              set_minutes_plus  => set_minutes_plus,
-              set_minutes_minus => set_minutes_minus,
-              hours             => hours,
-              minutes           => minutes,
-              seconds           => seconds);
+    port map (clk     => clk,
+              rst     => rst,
+              hours   => hours,
+              minutes => minutes,
+              seconds => seconds);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
 
-    -- Clock main signal
+
     clk <= TbClock;
 
-stimuli : process
-begin
-    -- Initialization
-    set_hours_plus    <= (others => '0');
-    set_hours_minus   <= (others => '0');
-    set_minutes_plus  <= (others => '0');
-    set_minutes_minus <= (others => '0');
-    rst               <= '0';
+    stimuli : process
+    begin
+    -- Reset the clock
+    rst <= '1';  -- rst activate
+    wait for 100 ns;  -- waiting 100 ns 
+    rst <= '0';  -- rst deactivate
+    wait for 100 ns;  -- waiting 100 ns 
 
-    -- Reset pulse
-    rst <= '1';
-    wait for 200 ns;
-    rst <= '0';
-    wait for 500 ns;
+    wait for 5 * TbPeriod;  -- 10 seconds (10 * period of clock signal)
 
-    -- Let the clock run for about 1 second
-    wait for 1 us;
-
-    -- Set hours +1 (set_hours_plus = "01")
-    set_hours_plus <= "01";
-    wait for TbPeriod;
-    set_hours_plus <= "00";
-    wait for 500 ns;
-
-    -- Set minutes +1 (set_minutes_plus = "01")
-    set_minutes_plus <= "01";
-    wait for TbPeriod;
-    set_minutes_plus <= "00";
-    wait for 500 ns;
-
-    -- Set hours -1 (set_hours_minus = "01")
-    set_hours_minus <= "01";
-    wait for TbPeriod;
-    set_hours_minus <= "00";
-    wait for 500 ns;
-
-    -- Set minutes -1 (set_minutes_minus = "01")
-    set_minutes_minus <= "01";
-    wait for TbPeriod;
-    set_minutes_minus <= "00";
-    wait for 500 ns;
-
-    -- Let the clock run for about 2 more seconds
-    wait for 2 us;
-
-    -- End simulation
-    TbSimEnded <= '1';
-    wait;
+    
+    TbSimEnded <= '1';  -- end simulation
+    wait;  
 end process;
 
 end tb;
--- Configuration block below is required by some simulators. Usually no need to edit.
 
 configuration cfg_tb_digital_clock of tb_digital_clock is
     for tb
